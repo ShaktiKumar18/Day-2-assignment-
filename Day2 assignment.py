63a3ae847bbf36571ebcc2d54ef1b1e4784f243b
ShaktiKumar18 <132076536+ShaktiKumar18@users.noreply.github.com>
2023-04-28 20:19:26 +0530

    Add files via upload

---
+++ Day2 assignment.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("a user inputted number {0} is Even".format(num))
+else:
+   print("a user inputted number {0} is Odd".format(num))
